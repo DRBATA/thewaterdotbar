f89d6ce60ee6d519c4da869a5a6bd4d27098e5fa
DRBATA <164792895+DRBATA@users.noreply.github.com>
2025-06-18 23:30:37 +0100

    feat: manually include pdfkit font files

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      // Ensure font files for the /api/order/receipt route are included
-      // This helps Vercel bundle the necessary .afm files for PDFKit.
-      '/api/order/receipt': ['./node_modules/pdfkit/js/data/**/*'],
-    },
-  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
